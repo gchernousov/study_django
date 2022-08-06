160d810d155d4d80fe010a7bd91344645034c105
gchernousov <seasidesuicide@yandex.ru>
2022-08-06 13:42:33 +0500

    task 2.1 database: show catalog, show product

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -7,7 +7,7 @@ class Phone(models.Model):
     price = models.IntegerField(verbose_name='Цена')
     release_date = models.DateField(verbose_name='Дата выхода')
     lte_exists = models.BooleanField(verbose_name='LTE')
-    slug = models.SlugField(max_length=64, db_index=True, verbose_name='URL')
+    slug = models.SlugField(max_length=64, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.name
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +8,12 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    context = {'phones': Phone.objects.all()}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
