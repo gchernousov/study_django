053a41a47c1758ba36b5bc0aee4986d930284c93
gchernousov <seasidesuicide@yandex.ru>
2022-09-14 15:56:28 +0500

    task 3.3: add draft

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q
 
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
@@ -11,11 +12,20 @@ from .filters import AdvertisementFilter, CreatorSearch, StatusSearch
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
 
-    queryset = Advertisement.objects.all()
+    queryset = Advertisement.objects.exclude(status="DRAFT")
     serializer_class = AdvertisementSerializer
     filter_backends = [CreatorSearch, StatusSearch, DjangoFilterBackend]
     search_fields = ['creator__id', 'status',]
     filterset_class = AdvertisementFilter
+
+
+    def get_queryset(self):
+        if str(self.request.user) != 'AnonymousUser':
+            queryset = Advertisement.objects.filter(
+                Q(creator=self.request.user.id) | Q(status="OPEN") | Q(status="CLOSED")
+            )
+            return queryset
+        return super().get_queryset()
 
     def get_permissions(self):
         """Получение прав для действий."""
